9fe056d2f620f114b33ed6e066ff2a3d4454ef05
Seth Zygarlicke <zygster11@gmail.com>
2025-12-28 14:40:37 -0600

    touched up the manual mode suggestion component.

--- frontend/src/components/form/BookForm/ManualModeSuggestion.vue
+++ frontend/src/components/form/BookForm/ManualModeSuggestion.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="manual-mode-suggestion">
         <h6>ain't finding your shit?</h6>
-        <a
-            v-if="latestQuery"
-            :href="`https://www.google.com/search?q=${latestQuery}-book`"
-            target="_blank"
-        >
-            use this link for deets, then enter them manually
-        </a>
+        <p>
+            use
+            <a
+                v-if="latestQuery"
+                :href="`https://www.google.com/search?q=${latestQuery}-book`"
+                target="_blank"
+                >this link</a
+            >
+            for deets, then enter them manually
+        </p>
         <BaseButton
             variant="outline"
             title="fuck it, we'll do it live"
@@ -59,7 +62,7 @@ h6 {
 
 p {
     font-size: 0.9rem;
-    color: var(--accent-blue);
+    color: var(--main-text);
 }
 
 a {
@@ -67,5 +70,23 @@ a {
     color: var(--accent-lavender);
     text-decoration: underline;
     cursor: pointer;
+}
+
+@media (min-width: 768px) {
+    .manual-mode-suggestion {
+        max-width: 75%;
+        align-self: center;
+        gap: 0.75rem;
+        padding: 1rem 2rem;
+    }
+    h6 {
+        font-size: 1.25rem;
+    }
+    p {
+        font-size: 1rem;
+    }
+    a {
+        font-size: 1rem;
+    }
 }
 </style>
