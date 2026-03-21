14c0d534b858c35abce63cefb77a420cbf829157
Seth Zygarlicke <zygster11@gmail.com>
2026-03-20 20:29:41 -0500

    more

--- frontend/src/components/views/ProseComposerView.vue
+++ frontend/src/components/views/ProseComposerView.vue
@@ -1,6 +1,6 @@
 <template>
     <AppLayout>
-        <ProseComposerV2 v-if="useV2" />
+        <ProseComposerV2 v-if="useV2ProseComposer" />
         <ProseComposerV1 v-else />
     </AppLayout>
 </template>
@@ -9,6 +9,5 @@
 import AppLayout from "@/components/layout/AppLayout.vue";
 import ProseComposerV1 from "@/components/features/Prose/Composer/ProseComposerV1.vue";
 import ProseComposerV2 from "@/components/features/Prose/Composer/ProseComposerV2.vue";
-
-const useV2 = false;
+import { useV2ProseComposer } from "@/constants/features";
 </script>
